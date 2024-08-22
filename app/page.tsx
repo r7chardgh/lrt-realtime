d98229dc1d2fc33e23a323dd79859b6669378679
richard tsang <richard.tsang9641@gmail.com>
2024-08-22 23:09:43 +0800

    Modify main page style

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
+
 import { fetchLrtDataByStationId } from "./lib/data";
 import Link from "next/link";
 import RouteList from "@/route.json"
 export const runtime = "edge";
 export default async function Home() {
   return (
-    <main>
-      {RouteList.map((r) => (<Link key={r} href={`/route/${r}`} className="p-6 bg-red-300 flex justify-center items-center gap-2"><p>{r}</p></Link>))}
+    <main className="flex justify-center items-center w-full p-12 ">
+      <div className="flex flex-col gap-4 flex-wrap w-full">
+
+      {RouteList.map((r) => (<Link key={r} href={`/route/${r}`} className="w-full flex-1 p-6  flex justify-center items-center gap-2"><p className="w-24 text-lg rounded-3xl border-4 border-green-400 flex justify-center outline">{r}</p></Link>))}
+      </div>
     </main>
   );
 }
