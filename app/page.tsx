d5ff523099e42eaf81dbb95bc1a41b24b013fb1b
richard tsang <richard.tsang9641@gmail.com>
2024-08-22 22:41:09 +0800

    Update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { fetchLrtDataByStationId } from "./lib/data";
 import Link from "next/link";
+import RouteList from "@/route.json"
 export const runtime = "edge";
 export default async function Home() {
   return (
     <main>
-      <Link href="/station/1">Tuen Mun Ferry Pier</Link>
+      {RouteList.map((r) => (<Link key={r} href={`/route/${r}`} className="p-6 bg-red-300 flex justify-center items-center gap-2"><p>{r}</p></Link>))}
     </main>
   );
 }
