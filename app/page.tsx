164d914883180e152ed728aadc8ca31263beccf2
richard tsang <richard.tsang9641@gmail.com>
2024-08-27 01:39:34 +0800

    Modify main page style

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
-
 import { fetchLrtDataByStationId } from "./lib/data";
 import Link from "next/link";
-import RouteList from "@/route.json"
+import RouteList from "@/route.json";
 export const runtime = "edge";
 export default async function Home() {
+  const colorVariants: any = {
+    505: "border-c_505",
+    507: "border-c_507",
+    610: "border-c_610",
+    614: "border-c_614",
+    "614p": "border-c_614p",
+    615: "border-c_615",
+    "615p": "border-c_615p",
+    705: "border-c_705",
+    706: "border-c_706",
+    751: "border-c_751",
+    "761p": "border-c_761p",
+  };
   return (
     <main className="flex justify-center items-center w-full p-12 ">
       <div className="flex flex-col gap-4 flex-wrap w-full">
-
-      {RouteList.map((r) => (<Link key={r} href={`/route/${r}`} className="w-full flex-1 p-6  flex justify-center items-center gap-2"><p className="w-24 text-lg rounded-3xl border-4 border-green-400 flex justify-center outline">{r}</p></Link>))}
+        {RouteList.map((r) => (
+          <Link
+            key={r}
+            href={`/route/${r}`}
+            className="w-full flex-1 p-6  flex justify-center items-center gap-2"
+          >
+            <p
+              className={`w-24 text-lg rounded-3xl border-4 flex justify-center outline ${
+                colorVariants[r.toLowerCase()]
+              }`}
+            >
+              {r}
+            </p>
+          </Link>
+        ))}
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: { lrt_yellow: "rgb(var(--lrt-yellow))", lrt_blue: "rgb(var(--lrt-blue))" },
+      colors: {
+        lrt_yellow: "rgb(var(--lrt-yellow))",
+        lrt_blue: "rgb(var(--lrt-blue))",
+        c_505:"rgb(var(--c-505))",
+        c_507:"rgb(var(--c-507))",
+        c_610:"rgb(var(--c-610))",
+        c_614:"rgb(var(--c-614))",
+        c_614p:"rgb(var(--c-614p))",
+        c_615:"rgb(var(--c-615))",
+        c_615p:"rgb(var(--c-615p))",
+        c_705:"rgb(var(--c-705))",
+        c_706:"rgb(var(--c-706))",
+        c_751:"rgb(var(--c-751))",
+        c_761p:"rgb(var(--c-761p))",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
