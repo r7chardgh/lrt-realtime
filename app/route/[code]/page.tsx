e050c7aa6e5450bb8c466198257f21e60cc56804
richard tsang <richard.tsang9641@gmail.com>
2024-09-02 22:51:45 +0800

    Add log

--- app/route/[code]/page.tsx
+++ app/route/[code]/page.tsx
@@ -8,6 +8,8 @@ import Link from "next/link";
 export default async function Page({ params }: { params: { code: string } }) {
   // const fetcher = (url: any) => fetch(url).then((r) => r.json());
   // const { data } = useSWR(`/api/route/${params.code}`, fetcher);
+  console.log(getUrl(`/api/route/${params.code}`));
+
   const data = await fetch(getUrl(`/api/route/${params.code}`)).then((res) =>
     res.json()
   );
