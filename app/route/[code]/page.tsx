aa1b92ad976e3b1af6ed59c77f7cf67708f60d63
richard tsang <richard.tsang9641@gmail.com>
2024-09-02 23:44:46 +0800

    log path

--- app/route/[code]/page.tsx
+++ app/route/[code]/page.tsx
@@ -8,11 +8,11 @@ import Link from "next/link";
 export default async function Page({ params }: { params: { code: string } }) {
   // const fetcher = (url: any) => fetch(url).then((r) => r.json());
   // const { data } = useSWR(`/api/route/${params.code}`, fetcher);
-  console.log(getUrl(`/api/route/${params.code}`));
+  console.log(process.env.NEXT_PUBLIC_URL + `/api/route/${params.code}`);
 
-  const data = await fetch(getUrl(`/api/route/${params.code}`)).then((res) =>
-    res.json()
-  );
+  const data = await fetch(
+    process.env.NEXT_PUBLIC_URL + `/api/route/${params.code}`
+  ).then((res) => res.json());
 
   const stop = RouteList.find((r: Stop) => {
     r.route_code;
