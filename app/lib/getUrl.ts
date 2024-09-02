7fd0ff3517375e0c634717612051799a3ede957d
richard tsang <richard.tsang9641@gmail.com>
2024-09-02 22:59:14 +0800

    Update production base url

--- app/lib/getUrl.ts
+++ app/lib/getUrl.ts
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://$NEXT_PUBLIC_VERCEL_URL`
     : "http://localhost:3000";
 
 const INCLUDES_FORWARD_SLASH_AT_START_REGEX = /^\/(.|\n)*$/;
